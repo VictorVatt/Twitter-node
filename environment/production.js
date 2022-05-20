0a55696a09e71def9503a64b0fca13392a35aaf5
Victor Vattier <victor.vattier35@gmail.com>
2022-05-20 14:24:22 +0200

    sef

--- environment/production.js
+++ environment/production.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     dbUrl: 'mongodb+srv://vatt:QygiI3r0mPHpKCqF@cluster0.4yn1d.mongodb.net/Twitter?retryWrites=true&w=majority',
-    cert: '/home/ubuntu/ssh/etc/letsencrypt/live/www.vatt-project.site/fullchain.pem',
-    key: '/home/ubuntu/ssh/etc/letsencrypt/live/www.vatt-project.site/privkey.pem',
+    cert: '/etc/letsencrypt/live/www.vatt-project.site/fullchain.pem',
+    key: '/etc/letsencrypt/live/www.vatt-project.site/privkey.pem',
 
 }
